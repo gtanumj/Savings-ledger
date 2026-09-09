06770b96417ccfcd227ce8bb0bf49ca2170855a9
gtanumj <gtanumj@gmail.com>
2026-09-10 00:47:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "savings-ledger-v1";
+const CACHE = "savings-ledger-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
